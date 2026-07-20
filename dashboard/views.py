6a9ce6227c16859303238b64eae3f1a68451db2b
Shetty Keerthi <keerthishetty580@gmail.com>
2026-07-20 09:57:50 +0530

    Complete authentication system and dashboard foundation

---
+++ dashboard/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, "dashboard/dashboard.html")
